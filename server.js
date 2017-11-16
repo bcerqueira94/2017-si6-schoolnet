4af904503f4cf7c2db34159265dfcb0c497b1bd1
Bruno Cerqueira <bcerqueira94@gmail.com>
2017-11-16 21:28:01 -0200

    Addedd index.ejs

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = 3000
 
-app.get('/', (request, response) => response.send('Hi there!'))
+//Express setup
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded ({ extended: false }))
+app.use(express.static('public'))
+
+//Templates
+app.set('views', path.join(__dirname, 'public'))
+app.set('view engine','ejs')
+
+app.get('/', (request, response) => {
+    response.render('index')
+})
 
 app.listen(port, () => {
     console.log('Server is running on port ' + port)
